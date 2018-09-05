6f39be6831d50ae7bfffa4bcf4cef5e49f535afe
pinigin <spinighin@mail.ru>
2018-09-05 12:19:18 +0300

    lesson2Tasks

---
+++ src/by/it/pinigin/lesson02/TaskA3.java
@@ -0,0 +1,15 @@
+package by.it.pinigin.lesson02;
+/*
+Измените переменные и строку вывода в программе так,
+чтобы она рассчитывала и печатала следующее выражение
+3*3+4*4=25
+ */
+
+class TaskA3 {
+    public static void main(String[] args) {
+        int i = 3*3;
+        int j = 4*4;
+        int k =  3*3+4*4 ;
+        System.out.println(i + "+" + j + "=" + k);
+    }
+}
