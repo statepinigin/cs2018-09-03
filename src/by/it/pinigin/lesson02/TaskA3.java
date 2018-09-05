f9296aa1d0b1c78003c2c84b7b08b68aa9f74202
pinigin <spinighin@mail.ru>
2018-09-05 12:37:43 +0300

    lesson2Tasks

--- src/by/it/pinigin/lesson02/TaskA3.java
+++ src/by/it/pinigin/lesson02/TaskA3.java
@@ -7,9 +7,9 @@ package by.it.pinigin.lesson02;
 
 class TaskA3 {
     public static void main(String[] args) {
-        int i = 3*3;
-        int j = 4*4;
-        int k =  3*3+4*4 ;
-        System.out.println(i + "+" + j + "=" + k);
+        int i = 3;
+        int j = 4;
+        int k =  i*i+j*j;
+        System.out.println(i +"*" +i+"+" +j+"*" + j + "=" + k);
     }
 }
